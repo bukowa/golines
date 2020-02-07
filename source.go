78d4027f0e53be866d1aa26bb3842f3daa30fd3a
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-02-07 05:55:57 +0100

    commit

--- source.go
+++ source.go
@@ -13,30 +13,101 @@ type Source struct {
 	bytes  []byte
 }
 
+// there must be a better way
 func (s *Source) Close() error {
 	return nil
 }
 
-func (s *Source) Read(b []byte)(n int, err error) {
+func (s *Source) Read(b []byte) (n int, err error) {
 	rd := bytes.NewReader(s.bytes)
 	return rd.Read(b)
 }
 
-func (s *Source) Write(b []byte) (n int, err error){
+// appends b to s.bytes with '\n'
+// there must be a better way
+func (s *Source) Write(b []byte) (n int, err error) {
 	s.SetBytes(bytes.Join([][]byte{s.bytes, b}, []byte("\n")))
 	return len(b), nil
 }
 
-func NewSource() *Source {
+func (s *Source) Add(x interface{}) (err error){
+	if t, ok := x.(string); ok {
+		_, err = s.Write([]byte(t))
+		return
+	}
+	if t, ok := x.([]byte); ok {
+		_, err = s.Write(t)
+		return
+	}
+	if t, ok := x.([][]byte); ok {
+		for _, l := range t {
+			_, err = s.Write(l)
+			if err != nil {
+				return err
+			}
+		}
+		return
+	}
+	if t, ok := x.([]string); ok {
+		for _, l := range t {
+			_, err = s.Write([]byte(l))
+			if err != nil {
+				return err
+			}
+		}
+		return
+	}
+	if t, ok := x.(Source); ok {
+		err = t.ForLine(func(i int, l []byte) error {
+			_, err = s.Write(l)
+			return err
+		})
+		return
+	}
+	return err
+}
+
+func NewSource(v interface{}) (s *Source, err error) {
+	s = &Source{}
+	if t, ok := v.(string); ok {
+		s.SetString(t)
+	}
+	if t, ok := v.([]byte); ok {
+		s.SetBytes(t)
+	}
+	if t, ok := v.([][]byte); ok {
+		for _, l := range t {
+			_, err = s.Write(l)
+			if err != nil {
+				return s, err
+			}
+		}
+	}
+	if t, ok := v.([]string); ok {
+		for _, l := range t {
+			_, err = s.Write([]byte(l))
+			if err != nil {
+				return s, err
+			}
+		}
+	}
+	if t, ok := v.(Source); ok {
+		err = t.ForLine(func(i int, l []byte) error {
+			_, err = s.Write(l)
+			return err
+		})
+	}
+	return s, nil
+}
+func NewSourceBytes(b []byte) *Source {
 	return &Source{
-		Parser: &BasicParser{},
+		bytes: b,
 	}
 }
 
-func NewSourceFrom(source string) *Source {
+func NewSourceString(s string) *Source {
 	return &Source{
-		Parser: &BasicParser{},
-		Source: source,
+		bytes: []byte(s),
 	}
 }
 
@@ -56,6 +127,55 @@ func (s *Source) Parse() (err error) {
 	return s.ParseSource(s.Source)
 }
 
+// how many lines are in bytes?
+func (s *Source) CountBytes(in []byte) (c int) {
+	for _, v := range s.ByteLines() {
+		if bytes.ContainsAny(v, string(in)) {
+			c += 1
+		}
+	}
+	return c
+}
+
+// how many lines are in string?
+func (s *Source) CountString(in string) (c int) {
+	for _, v := range s.StringLines() {
+		if strings.ContainsAny(in, v) {
+			c += 1
+		}
+	}
+	return c
+}
+
+// map of line => count
+func (s *Source) CountStringMapLineN(in string) map[string]int {
+	var d = make(map[string]int)
+	for _, v := range s.StringLines() {
+		d[v] = strings.Count(in, v)
+	}
+	return d
+}
+
+// map of count => [lines...] ; keys are unordered
+func (s *Source) CountStringMapNLines(in string) map[int][]string {
+	var d = make(map[int][]string)
+	for _, l := range s.StringLines() {
+		c := strings.Count(in, l)
+		d[c] = append(d[c], l)
+	}
+	return d
+}
+
+// map of count => [lines...] ; keys are unordered
+func (s *Source) CountBytesMapNLines(in []byte) map[int][][]byte {
+	var d = make(map[int][][]byte)
+	for _, l := range s.ByteLines() {
+		c := bytes.Count(in, l)
+		d[c] = append(d[c], l)
+	}
+	return d
+}
+
 func (s *Source) ParseSource(source string) (err error) {
 	var b []byte
 	for key, action := range s.Parser.PrefixMap() {
@@ -70,7 +190,7 @@ func (s *Source) ParseSource(source string) (err error) {
 	return err
 }
 
-func (s *Source) ForLine(x func(int, []byte) error) (err error) {
+func (s *Source) ForLine(x func(i int, l []byte) error) (err error) {
 	reader := bytes.NewReader(s.Bytes())
 	scanner := bufio.NewScanner(reader)
 
@@ -93,36 +213,37 @@ func (s *Source) String() string {
 	return string(s.bytes)
 }
 
-func (s *Source) ByteLines() (data [][]byte, err error) {
-	err = s.ForLine(func(i int, l []byte) error {
+func (s *Source) ByteLines() (data [][]byte) {
+	s.ForLine(func(i int, l []byte) error {
 		data = append(data, l)
 		return nil
 	})
-	return data, err
+	return
 }
 
-func (s *Source) ByteLinesPreSuf(pre, suf []byte) (data [][]byte, err error) {
-	err = s.ForLine(func(i int, l []byte) error {
+func (s *Source) ByteLinesPreSuf(pre, suf []byte) (data [][]byte) {
+	s.ForLine(func(i int, l []byte) error {
+		// there must be a better way
 		data = append(data, bytes.Join([][]byte{pre, l, suf}, nil))
 		return nil
 	})
-	return data, err
+	return
 }
 
-func (s *Source) StringLines() (data []string, err error) {
-	err = s.ForLine(func(i int, l []byte) error {
+func (s *Source) StringLines() (data []string) {
+	s.ForLine(func(i int, l []byte) error {
 		data = append(data, string(l))
 		return nil
 	})
-	return data, err
+	return
 }
 
-func (s *Source) StringLinesPreSuf(pre, suf string) (data []string, err error) {
-	err = s.ForLine(func(i int, l []byte) error {
+func (s *Source) StringLinesPreSuf(pre, suf string) (data []string) {
+	s.ForLine(func(i int, l []byte) error {
 		data = append(data, pre+string(l)+suf)
 		return nil
 	})
-	return data, err
+	return
 }
 
 func (s *Source) IntLines() (data []int, err error) {
@@ -134,5 +255,5 @@ func (s *Source) IntLines() (data []int, err error) {
 		data = append(data, i)
 		return err
 	})
-	return data, err
+	return
 }
--- source_test.go
+++ source_test.go
@@ -8,6 +8,140 @@ import (
 	"testing"
 )
 
+var checkDeepEqual = func(t *testing.T, x, desired interface{}) {
+	if !reflect.DeepEqual(x, desired) {
+		t.Errorf("'%v' IS NOT '%v'", x, desired)
+	}
+}
+
+func TestSource_Write(t *testing.T) {
+	var b = []byte("1\n2\n3")
+	s := &Source{}
+	s.SetBytes(b)
+	_, err := s.Write([]byte("4"))
+	if err != nil {
+		panic(err)
+	}
+	data := s.StringLines()
+	if len(data) != 4 {
+		t.Error(data)
+	}
+	if data[3] != "4" {
+		t.Error(data[3])
+	}
+}
+
+func TestNewSourceFromBytes(t *testing.T) {
+	var b = []byte("1\n2\n3")
+	s := NewSourceBytes(b)
+	checkDeepEqual(t, s.bytes, b)
+}
+
+func TestNewSourceFromString(t *testing.T) {
+	var str = "1\n2\n3"
+	var desired = []byte(str)
+	s := NewSourceString(str)
+	checkDeepEqual(t, s.bytes, desired)
+}
+
+func TestSource_SetSource(t *testing.T) {
+	var desired = "test"
+	s := &Source{}
+	s.SetSource(desired)
+	checkDeepEqual(t, s.Source, desired)
+}
+
+func TestSource_SetBytes(t *testing.T) {
+	var desired = []byte("byte")
+	s := &Source{}
+	s.SetBytes(desired)
+	checkDeepEqual(t, s.bytes, desired)
+}
+
+func TestSource_SetString(t *testing.T) {
+	var str = "test"
+	var desired = []byte(str)
+	s := &Source{}
+	s.SetString(str)
+	checkDeepEqual(t, s.bytes, desired)
+}
+
+//func TestSource_Parse(t *testing.T) {
+//	s := &Source{}
+//}
+func TestSource_CountBytes(t *testing.T) {
+	var b = []byte("1\n2\n3")
+	var in = []byte("123456")
+	s := NewSourceBytes(b)
+	c := s.CountBytes(in)
+	if c != 3 {
+		t.Error(c)
+	}
+	var in2 = []byte("12")
+	c = s.CountBytes(in2)
+	if c != 2 {
+		t.Error(c)
+	}
+}
+
+func TestSource_CountString(t *testing.T) {
+	var str = "1\n2\n3"
+	var in = "123456"
+	s := NewSourceString(str)
+	c := s.CountString(in)
+	if c != 3 {
+		t.Error()
+	}
+	var in2 = "12"
+	c = s.CountString(in2)
+	if c != 2 {
+		t.Error()
+	}
+}
+
+func TestSource_CountStringMapLineN(t *testing.T) {
+	var str = "1\n2\n3"
+	var in = "112223334"
+	var desired = map[string]int{"1": 2, "2": 3, "3":3}
+
+	s := NewSourceString(str)
+	m := s.CountStringMapLineN(in)
+
+	if !reflect.DeepEqual(m, desired) {
+		t.Error(m)
+	}
+}
+
+// this may fail, because order is not guaranteed
+// we should sort the keys later
+func TestSource_CountStringMapNLines(t *testing.T) {
+	var str = "1\n2\n3\n4\n5\n6"
+	var in = "112223334"
+	var desired = map[int][]string{
+		0: {"5", "6"},
+		2: {"1"},
+		3: {"2", "3"},
+		1: {"4"},
+	}
+	s := NewSourceString(str)
+	m := s.CountStringMapNLines(in)
+	checkDeepEqual(t, m, desired)
+}
+
+func TestSource_CountBytesMapNLines(t *testing.T) {
+	var b = []byte("1\n2\n3\n4\n5\n6")
+	var in = []byte("112223334")
+	var desired = map[int][][]byte{
+		0: {[]byte("5"), []byte("6")},
+		2: {[]byte("1")},
+		3: {[]byte("2"), []byte("3")},
+		1: {[]byte("4")},
+	}
+	s := NewSourceBytes(b)
+	m := s.CountBytesMapNLines(in)
+	checkDeepEqual(t, m, desired)
+}
+
 func TestSourceBytesStringsLines(t *testing.T) {
 	s := &Source{
 		bytes: []byte("1\n2\n3"),
@@ -15,15 +149,13 @@ func TestSourceBytesStringsLines(t *testing.T) {
 	if !reflect.DeepEqual(s.Bytes(), []byte("1\n2\n3")) {
 		t.Error()
 	}
-	byteLines, _ := s.ByteLines()
-	if !reflect.DeepEqual(byteLines, [][]byte{[]byte("1"), []byte("2"), []byte("3")}) {
+	if !reflect.DeepEqual(s.ByteLines(), [][]byte{[]byte("1"), []byte("2"), []byte("3")}) {
 		t.Error()
 	}
 	if s.String() != "1\n2\n3" {
 		t.Error()
 	}
-	stringLines, _ := s.StringLines()
-	if !reflect.DeepEqual(stringLines, []string{"1", "2", "3"}) {
+	if !reflect.DeepEqual(s.StringLines(), []string{"1", "2", "3"}) {
 		t.Error()
 	}
 	intLines, _ := s.IntLines()
@@ -32,7 +164,6 @@ func TestSourceBytesStringsLines(t *testing.T) {
 	}
 }
 
-// file://
 func TestSource_ParseFile(t *testing.T) {
 	var b = []byte("1\n2\n3")
 	var ts = httptest.NewServer(http.FileServer(http.Dir(".")))
@@ -55,15 +186,13 @@ func TestSource_ParseFile(t *testing.T) {
 	if !reflect.DeepEqual(s.Bytes(), []byte("1\n2\n3")) {
 		t.Error()
 	}
-	byteLines, _ := s.ByteLines()
-	if !reflect.DeepEqual(byteLines, [][]byte{[]byte("1"), []byte("2"), []byte("3")}) {
+	if !reflect.DeepEqual(s.ByteLines(), [][]byte{[]byte("1"), []byte("2"), []byte("3")}) {
 		t.Error()
 	}
 	if s.String() != "1\n2\n3" {
 		t.Error()
 	}
-	stringLines, _ := s.StringLines()
-	if !reflect.DeepEqual(stringLines, []string{"1", "2", "3"}) {
+	if !reflect.DeepEqual(s.StringLines(), []string{"1", "2", "3"}) {
 		t.Error()
 	}
 	intLines, _ := s.IntLines()
@@ -97,15 +226,13 @@ func TestSource_ParseHttp(t *testing.T) {
 	if !reflect.DeepEqual(s.Bytes(), []byte("1\n2\n3")) {
 		t.Error()
 	}
-	byteLines, _ := s.ByteLines()
-	if !reflect.DeepEqual(byteLines, [][]byte{[]byte("1"), []byte("2"), []byte("3")}) {
+	if !reflect.DeepEqual(s.ByteLines(), [][]byte{[]byte("1"), []byte("2"), []byte("3")}) {
 		t.Error()
 	}
 	if s.String() != "1\n2\n3" {
 		t.Error()
 	}
-	stringLines, _ := s.StringLines()
-	if !reflect.DeepEqual(stringLines, []string{"1", "2", "3"}) {
+	if !reflect.DeepEqual(s.StringLines(), []string{"1", "2", "3"}) {
 		t.Error()
 	}
 	intLines, _ := s.IntLines()
@@ -113,50 +240,24 @@ func TestSource_ParseHttp(t *testing.T) {
 		t.Error()
 	}
 
-	byteLinesPreSuf, _ := s.ByteLinesPreSuf([]byte("a"), []byte("b"))
-	if !reflect.DeepEqual(byteLinesPreSuf, [][]byte{[]byte("a1b"), []byte("a2b"), []byte("a3b")}) {
+	if !reflect.DeepEqual(s.ByteLinesPreSuf([]byte("a"), []byte("b")), [][]byte{[]byte("a1b"), []byte("a2b"), []byte("a3b")}) {
 		t.Error()
 	}
-	stringLinesPreSuf, _ := s.StringLinesPreSuf("a", "b")
-	if !reflect.DeepEqual(stringLinesPreSuf, []string{"a1b", "a2b", "a3b"}) {
-		t.Error()
-	}
-
-	byteLinesPreSuf2, _ := s.ByteLinesPreSuf([]byte("a"), nil)
-	if !reflect.DeepEqual(byteLinesPreSuf2, [][]byte{[]byte("a1"), []byte("a2"), []byte("a3")}) {
-		t.Error()
-	}
-	stringLinesPreSuf2, _ := s.StringLinesPreSuf("a", "")
-	if !reflect.DeepEqual(stringLinesPreSuf2, []string{"a1", "a2", "a3"}) {
+	if !reflect.DeepEqual(s.StringLinesPreSuf("a", "b"), []string{"a1b", "a2b", "a3b"}) {
 		t.Error()
 	}
 
-	byteLinesPreSuf3, _ := s.ByteLinesPreSuf(nil, []byte("b"))
-	if !reflect.DeepEqual(byteLinesPreSuf3, [][]byte{[]byte("1b"), []byte("2b"), []byte("3b")}) {
+	if !reflect.DeepEqual(s.ByteLinesPreSuf([]byte("a"), nil), [][]byte{[]byte("a1"), []byte("a2"), []byte("a3")}) {
 		t.Error()
 	}
-	stringLinesPreSuf3, _ := s.StringLinesPreSuf("", "b")
-	if !reflect.DeepEqual(stringLinesPreSuf3, []string{"1b", "2b", "3b"}) {
+	if !reflect.DeepEqual(s.StringLinesPreSuf("a", ""), []string{"a1", "a2", "a3"}) {
 		t.Error()
 	}
-}
 
-func TestSource_Write(t *testing.T) {
-	var b = []byte("1\n2\n3")
-	s := NewSource()
-	s.SetBytes(b)
-	_, err := s.Write([]byte("4"))
-	if err != nil {
-		panic(err)
+	if !reflect.DeepEqual(s.ByteLinesPreSuf(nil, []byte("b")), [][]byte{[]byte("1b"), []byte("2b"), []byte("3b")}) {
+		t.Error()
 	}
-	data, err := s.StringLines()
-	if err != nil {
-		panic(err)
-	}
-	if len(data) != 4 {
-		t.Error(data)
-	}
-	if data[3] != "4" {
-		t.Error(data[3])
+	if !reflect.DeepEqual(s.StringLinesPreSuf("", "b"), []string{"1b", "2b", "3b"}) {
+		t.Error()
 	}
 }
